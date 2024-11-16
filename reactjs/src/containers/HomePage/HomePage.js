559479187654388b4c36ef0e2df213d2fe6e88cf
vongocanh03 <148541157+vongocanh03@users.noreply.github.com>
2024-11-16 18:35:44 +0700

    test git 1

--- reactjs/src/containers/HomePage/HomePage.js
+++ reactjs/src/containers/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 class HomePage extends Component {
-
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     render() {
 
         return (
