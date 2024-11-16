f17b2f3224723bf5a485d5d46d8ab0dcad105648
linhcutte <97034479+linhcutte@users.noreply.github.com>
2024-11-16 18:20:52 +0700

    xoa cmd

--- reactjs/src/containers/HomePage/HomeHeader.js
+++ reactjs/src/containers/HomePage/HomeHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss'
-//dasdasdasd//
+
 class HomeHeader extends Component {
 
     render() {
